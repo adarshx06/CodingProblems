74b71ce13ea627f75d9df8f9b39e89fb5e24a23e
Adarsh Thoke <37337770+adarshx06@users.noreply.github.com>
2024-01-03 13:56:21 -0700

    Create countInversion.java

---
+++ countInversion.java
@@ -0,0 +1,69 @@
+// Brute Force Approach TC: O(N2) SC: O(1)
+
+class Solution{
+  public static int countInversion(int[] arr){
+    int n = arr.length;
+    int count=0;
+    for(int i =0; i<n;i++){
+      for(int j =i+1; j<n; j++){
+        if(arr[i]>a[j]){
+          count++;
+      }
+    }
+      return count;
+  }
+}
+
+// Optimal Approach TC: O(nlogn) SC: O(n) [use temp arr]
+
+class Solution {
+  public static int countInversion(int[] arr) {
+    int n = arr.length;
+    int[] temp = new int[n];
+    int ans = mergeSort(arr, temp, 0, n - 1);
+    System.out.println(ans);
+    return ans;
+  }
+
+  static int mergeSort(int[] arr, int[] temp, int left, int right) {
+    int mid = 0;
+    int inv_count = 0;
+    if (right > left) {
+      mid = (right + left) / 2;
+      inv_count += mergeSort(arr, temp, left, mid);
+      inv_count += mergeSort(arr, temp, mid + 1, right);
+      inv_count += merge(arr, temp, left, mid + 1, right);
+    }
+    return inv_count;
+  }
+
+  static int merge(int[] arr, int[] temp, int left, int mid, int right) {
+    int i = left;
+    int j = mid;
+    int k = left;
+    int inv_count = 0;
+
+    while ((i <= mid - 1) && (j <= right)) {
+      if (arr[i] <= arr[j]) {
+        temp[k++] = arr[i++];
+      } else {
+        temp[k++] = arr[j++];
+        inv_count += (mid - i);
+      }
+    }
+
+    while (i <= mid - 1) {
+      temp[k++] = arr[i++];
+    }
+
+    while (j <= right) {
+      temp[k++] = arr[j++];
+    }
+
+    for (i = left; i <= right; i++) {
+      arr[i] = temp[i];
+    }
+
+    return inv_count;
+  }
+}
