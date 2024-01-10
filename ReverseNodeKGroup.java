bd20c8dfc3b5b6690918d7efb847c7fe684682c7
Adarsh Thoke <37337770+adarshx06@users.noreply.github.com>
2024-01-09 23:34:46 -0700

    Create ReverseNodeKGroup.java

---
+++ ReverseNodeKGroup.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        //base case 
+        if (head == null || k == 1) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+  
+        ListNode start = dummy;
+        ListNode end = head;
+        int count = 0;
+        while (end != null) {
+            count++;
+            // group
+            if (count % k == 0) {
+            // reverse linked list (start, end]
+            start = reverse(start, end.next);
+            end = start.next;
+            } else {
+            end = end.next;
+            }
+        }
+        return dummy.next;
+    }
+    
+    private ListNode reverse(ListNode start, ListNode end) {
+      ListNode curr = start.next;
+      ListNode prev = start;
+      ListNode first = curr;
+      while (curr != end){
+        ListNode temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+      }
+      start.next = prev;
+      first.next = curr;
+      return first;
+    }
+}
