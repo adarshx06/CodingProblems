df6f0bf4db6fff70c1431934b27e847cb5b0d5be
Adarsh Thoke <37337770+adarshx06@users.noreply.github.com>
2024-01-17 17:48:31 -0700

    Update inorderTraversal.java

--- trees_Problems/inorderTraversal.java
+++ trees_Problems/inorderTraversal.java
@@ -1,1 +1,35 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root== null){
+            return ans;
+        }
+        inorderTraversalHelper(root, ans);
+        return ans;
+        
+    }
+    private void inorderTraversalHelper(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        inorderTraversalHelper(node.left, result);
+        result.add(node.val);
+        inorderTraversalHelper(node.right, result);
+    }
+}
