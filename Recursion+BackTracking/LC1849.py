518a6f859421b48d0eca658d924ca5634c37368a
Adarsh Thoke <37337770+adarshx06@users.noreply.github.com>
2024-02-27 22:48:17 -0700

    Create LC1849.py

---
+++ Recursion+BackTracking/LC1849.py
@@ -0,0 +1,23 @@
+#Splitting a String Into Descending Consecutive Values
+
+class Solution:
+    def splitString(self, s: str) -> bool:
+        n=len(s)
+
+        def go(i,prev):
+            if(i==n):
+                return True
+            
+            valid = False
+            for index in range(i+1, n+1):
+                if int(s[i:index]) == prev -1:
+                    valid = go(index,prev-1)
+                if valid:
+                    return True
+            return valid
+        
+        for i in range(1,n):
+            if go(i, int(s[:i])):
+                return True
+
+        return False
